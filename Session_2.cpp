90facfe51375d0dee8ba9dbd969aeb47d0e34ad7
Pranav Sundar <110410334+pranavsundar08@users.noreply.github.com>
2022-09-01 22:55:18 +0530

    Add files via upload

---
+++ Session_2.cpp
@@ -0,0 +1,15 @@
+// Write a program using cout.//
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string name;
+	cout<<"+"<< name << endl;
+	cout<<"++"<< name << endl;
+	cout<<"+++"<< name << endl;
+	
+	return 0;
+}
