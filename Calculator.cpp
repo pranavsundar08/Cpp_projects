fd3fa22c37a248468cd6e881c3bb697e0d8d0a62
Pranav Sundar <pranavsundar5039@gmail.com>
2022-09-08 18:13:51 +0530

    2nd commit command

---
+++ Calculator.cpp
@@ -0,0 +1,41 @@
+//General calculator using two inputs.
+
+#include<iostream>
+#include<string>
+using namespace std;
+
+int add(int x,int y)
+{
+	return x+y;
+	
+}
+ int subtract(int x,int y)
+ {
+ 	return x-y;
+ }
+ int multiply(int x,int y)
+ {
+ 	return x*y;
+
+ }
+  int main()
+  int x,y;
+  {
+  	string operation;
+  	cout<<"Please enter value of x: ";
+  	cin>>x;
+  	cout<<"Please enter the value of y: ";
+  	cin>>y;
+  	cout<<"Operation to be performed: ";
+  	getline(cin,operation);
+  	if (operation==add)
+{	
+	cout<<"The addition of x and y are ="<<add<<endl;
+}	if else(operation==subtraction);
+{
+	cout<<"The subtraction of x and y are= "<<subtract<<endl;
+}	else
+{
+	cout<<"The multiplication of x and y are= "<<multiply<<endl;
+}
+  }
