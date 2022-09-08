36dcb33fd48c2f4955b54f5c42c6fa4d35ed62b4
Pranav Sundar <pranavsundar5039@gmail.com>
2022-09-08 11:35:20 +0530

    First commit

---
+++ simple.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+string name;
+int age, phone;
+cout<<"Please enter your name: "<<"\n";
+cin>>name;
+cout<<"Please enter your age: ";
+cin>>age;
+cout<<"Please enter your no. :";
+cin>>phone;
+cout<<"The name of the person is: "<<name <<endl;
+cout<<"The age of the person is: "<< age<<endl;
+cout<<"Your number is: "<<phone;
+return age;
+return phone;
+}
+
